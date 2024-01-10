6dacf364b60bb6ddb527558ccc28fdec0782ab87
sahil-ansari01 <opensourcecarrier@gmail.com>
2024-01-10 20:51:13 +0530

    obj

--- play.js
+++ play.js
@@ -1,20 +1,11 @@
-const name = 'Sahil';
-let age = 22;
-const hasHobbies = true;
-
-age = 23;
-
-const summarizeUser = (userName, userAge, userHobbies) => {
-    return (
-        'Name is ' + userName + ", age is " + userAge + " and has hobbies: " + userHobbies 
-    )
+const student = {
+    name : 'Sahil',
+    roll: 77,
+    class: 15,
+    present(){
+        console.log(this.name + ' is present')
+    }
 }
 
-// console.log(summarizeUser(name, age, hasHobbies));
+student.present();
 
-const num1 = 5;
-const num2 = 5;
-
-const product = (num1, num2) => num1 * num2;
-
-console.log(product(num1, num2));
