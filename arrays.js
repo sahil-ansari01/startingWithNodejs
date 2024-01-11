64cfcb83f8892a65fe4c3d53febf04d022ae06b0
sahil-ansari01 <opensourcecarrier@gmail.com>
2024-01-11 16:36:42 +0530

    destructuring

--- arrays.js
+++ arrays.js
@@ -18,3 +18,25 @@ const toArray = (...args) => {
 }
 
 console.log(1,2,3,4);
+
+// Destructuring
+
+const person = {
+    name: 'Sahil',
+    age: '23',
+    greet() {
+        console.log('Hi, I am ' + this.name);
+    }
+}
+
+const printName = ({ name }) => {
+    console.log(name);
+}
+
+printName(person);
+
+const { name, age} = person;
+console.log(name, age);
+
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1, hobby2)
